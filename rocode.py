63b701a80454f9382d47bec6430e839e6ba8829c
aBoredDev <rmunro2324@gmail.com>
2022-02-25 09:44:59 -0800

    Switched to using python's logging module  - Added a logger for each file  - Changed all print statements to the appropriate logging calls

--- rocode.py
+++ rocode.py
@@ -2,10 +2,17 @@ import datetime
 import pytz
 import random
 import discord
+import logging
 
 from random import shuffle
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from discord.ext import commands
+
+FORMAT = ('%(asctime)-15s %(threadName)-15s '
+          '%(levelname)-8s %(module)-15s:%(lineno)-8s %(message)s')
+logging.basicConfig(format=FORMAT)
+logger = logging.getLogger('RoCode')
+logger.setLevel(logging.INFO)
 
 
 class Rocode(commands.Cog):
@@ -14,7 +21,7 @@ class Rocode(commands.Cog):
         self.tz = pytz.timezone(bot.timezone_str)  # https://www.youtube.com/watch?v=-5wpm-gesOY
         self.epoch = datetime.datetime.strptime(bot.epoch_str, "%M %H %d %B %Y")
         self.epoch = self.tz.localize(self.epoch)
-        print("Rocode set to send messages at every ", bot.rocode_hour, ":", bot.rocode_minute, self.tz)
+        logger.info(f"Rocode set to send messages at every {bot.rocode_hour}:{bot.rocode_minute}{self.tz}")
 
         codefile = open("codes.txt")
         self.codes = codefile.readlines()
@@ -29,30 +36,30 @@ class Rocode(commands.Cog):
         self.bot = bot
 
         scheduler = AsyncIOScheduler()
-        scheduler.add_job(self.perform_job, trigger='cron', minute=bot.rocode_minute, hour=bot.rocode_hour, day='*', week='*', month='*',
-                          year='*', misfire_grace_time=100, coalesce=False, timezone=self.tz)
+        scheduler.add_job(self.perform_job, trigger='cron', minute=bot.rocode_minute, hour=bot.rocode_hour, day='*',
+                          week='*', month='*', year='*', misfire_grace_time=100, coalesce=False, timezone=self.tz)
         scheduler.start()
 
     async def perform_job(self):
-        print("Performing Rocode Job at " + datetime.datetime.now(tz=self.tz).strftime("%d-%m-%Y--%H-%M"))
+        logger.info("Performing Rocode Job at " + datetime.datetime.now(tz=self.tz).strftime("%d-%m-%Y--%H-%M"))
         curr_code = self.codes[(datetime.datetime.now(tz=self.tz) - self.epoch).days % len(self.codes)]
         for server, channel in self.rocodeChannel.items():
             try:
                 ch = self.bot.get_channel(channel)
                 if ch is None:
-                    print("Skipping server with no perms or non-existent channel ID")
+                    logger.info("Skipping server with no perms or non-existent channel ID")
                 else:
                     await self.bot.get_channel(channel).send(curr_code)
             except discord.HTTPException:
-                print("Could not send ro'code, HTTP error")
+                logger.warning("Could not send ro'code, HTTP error")
             except discord.Forbidden:
-                print("Could not send ro'code, Forbidden error")
+                logger.warning("Could not send ro'code, Forbidden error")
 
     # Users can manually retrieve the current rocode using this command in discord
     @commands.command(pass_context=True)
     async def rocode(self, ctx):
         curr_code = self.codes[(datetime.datetime.now(tz=self.tz) - self.epoch).days % len(self.codes)]
-        print((datetime.datetime.now(tz=self.tz) - self.epoch).days)
+        logger.info((datetime.datetime.now(tz=self.tz) - self.epoch).days)
         await ctx.channel.send("Today's Rover Code is:\n\n" + curr_code)
 
 
